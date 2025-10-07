e4a532ce0f9ac6191970cd6ece572a3e72e47274
vlnn <vlnn-github@proton.me>
2025-10-07 12:11:50 +0300

    Rearrange cells in day01

--- aoc_2015/day_01.py
+++ aoc_2015/day_01.py
@@ -7,29 +7,13 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    return (mo,)
-
-
-@app.cell
-def _(mo):
     part_dropdown = mo.ui.dropdown(
         options=[1,2],
         value=1,
         label="Part"
     )
-    return (part_dropdown,)
-
-
-@app.cell
-def _(part_dropdown):
     part_dropdown
-    return
-
-
-@app.cell
-def _(part_dropdown):
-    part = part_dropdown.value
-    return (part,)
+    return (part_dropdown,)
 
 
 @app.cell
@@ -37,14 +21,19 @@ def _():
     from itertools import accumulate
     def read_input(_,_1):
         return "(())(())((((()()))))))"
-
-    def solve_part1(directions) -> int:
-        lefts = directions.count("(")
-        rights = directions.count(")")
-
-        return lefts - rights
+    return accumulate, read_input
 
 
+@app.function
+def solve_part1(directions) -> int:
+    lefts = directions.count("(")
+    rights = directions.count(")")
+
+    return lefts - rights
+
+
+@app.cell
+def _(accumulate):
     def parse_as_ints(directions) -> list[int]:
         ints = [1 if c == "(" else -1 for c in directions]
         return ints
@@ -54,21 +43,28 @@ def _():
         ints = parse_as_ints(directions)
         accs = list(accumulate(ints))
         return accs.index(-1) + 1
-
-
-    def part1() -> int:
-        raw_input = read_input(2015, 1)
-        return solve_part1(raw_input)
-
-
-    def part2() -> int | None:
-        raw_input = read_input(2015, 1)
-        return solve_part2(raw_input)
-    return part1, part2
+    return (solve_part2,)
 
 
 @app.cell
-def _(part, part1, part2):
+def _(read_input):
+    def part1() -> int:
+        raw_input = read_input(2015, 1)
+        return solve_part1(raw_input)
+    return (part1,)
+
+
+@app.cell
+def _(read_input, solve_part2):
+    def part2() -> int | None:
+        raw_input = read_input(2015, 1)
+        return solve_part2(raw_input)
+    return (part2,)
+
+
+@app.cell
+def _(part1, part2, part_dropdown):
+    part = part_dropdown.value
     if part == 1:
         res = part1()
     else:
